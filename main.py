2375602b2edb3e9e917a2f58091b63ae917399a8
Jake Norris <jcoolman.norris@gmail.com>
2022-02-03 00:39:03 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ while game_is_on:
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
